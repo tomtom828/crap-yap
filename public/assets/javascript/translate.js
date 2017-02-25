f591c1b18178b17f4b9b46fdde8c7ff56748dfb9
Steven Freeman <sfreeman422@gmail.com>
2017-02-25 13:16:55 -0500

    Translation is being stored in the database upon entry now

--- public/assets/javascript/translate.js
+++ public/assets/javascript/translate.js
@@ -79,42 +79,31 @@ var transChart = {
 	}
 }
 
-function translate (data){
-	console.log("in the translate funciton");
-	console.log(data);
-	var dataCollection = data; 
-	for(var key in dataCollection){
-		//Skip loop if property is from the prototype
-		if(!dataCollection.hasOwnProperty(key)) continue;
-		//The item within the collection of objects that we are concerning ourselves with this time. 
-		var dataItem = dataCollection[key];
-		//Each of the properties of the object. 
-		console.log(dataItem.author);
-		console.log(dataItem.message);
-		console.log(dataItem.time);
-		//This is what we will return at the end. It should be a string of morse code. 
-		var morseTranslation = "";
-		//For loop to check the message and loop through the letters within the string. This is where actual translation begins.
-		for(var i = 0; i < dataItem.message.length; i++){
-			//Grab the letter that we are working with in this loop
-			var letter = dataItem.message[i]
-			//Make it lowercase so we can check it out in the KVP above. 
-			letter = letter.toLowerCase();
-			console.log("Letter is: "+letter);
-			//If we don't have the letter listed in the chart, it just becomes the same letter. No need to change to morse
-			if(transChart[letter] == undefined){
-				letter = letter; 
-				console.log(letter);
-				morseTranslation += letter; 
-			}
-			//Otherwise, were going to convert to morse. 
-			else{
-				letter = transChart[letter].morse;
-				console.log(letter);
-				morseTranslation += letter; 
-			}
+function translate (message){
+	console.log("in the translate functionnn");
+	console.log(message);
+	//This is what we will return at the end. It should be a string of morse code. 
+	var morseTranslation = "";
+	//For loop that goes through each message and converts to morse. This is where translation occurs.  
+	for(var i = 0; i < message.length; i++){
+		//Grab the letter that we are working with in this loop
+		var letter = message[i]
+		//Make it lowercase so we can check it out in the KVP above. 
+		letter = letter.toLowerCase();
+		console.log("Letter is: "+letter);
+		//If we don't have the letter listed in the chart, it just becomes the same letter. No need to change to morse
+		if(transChart[letter] == undefined){
+			letter = letter; 
+			console.log(letter);
+			morseTranslation += letter; 
 		}
-		//Return our fully translated phrase. 
-		return morseTranslation;
+		//Otherwise, were going to convert to morse. 
+		else{
+			letter = transChart[letter].morse;
+			console.log(letter);
+			morseTranslation += letter; 
+		}
 	}
+	//Return our fully translated phrase. 
+	return morseTranslation;
 }
